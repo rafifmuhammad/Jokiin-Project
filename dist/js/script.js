8b4ab4bfb82c82e648de182c222310358a4bfc75
Rafif Muhammad <52074659+rafifmuhammad@users.noreply.github.com>
2025-05-28 23:15:21 +0700

    Apakah Tim Memiliki Komitmen?
    Well, pada dasarnya saya memilki tim dalam project ini yang tidak satupun dari mereka melakukan coding, desain, atau melakukan pengujian. Ntahlah saya saat ini cukup merasa bahwa projek ini kemungkinan hanya akan menjadi projek gagal seperti milik saya yang lainnya. Apa yang mereka lakukan? Lebih ke promosi dan ntahlah Tuhan yang tau.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,12 @@
 const menuNav = document.querySelector('.menu-navigation');
 const menuBar = document.querySelector('.menu-bar');
+const headerShrink = document.querySelector('.header');
+const dashboardContentShrink = document.querySelector('.dashboard-content');
+const footerShrink = document.querySelector('.footer');
 
 menuBar.addEventListener('click', () => {
     menuNav.classList.toggle('menu-active');
+    headerShrink.classList.toggle('shrink');
+    dashboardContentShrink.classList.toggle('shrink')
+    footerShrink.classList.toggle('shrink');
 });
