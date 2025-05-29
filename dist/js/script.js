4cb7776118742fc2ca8687150c467dcd067b1c3f
Rafif Muhammad <52074659+rafifmuhammad@users.noreply.github.com>
2025-05-29 17:42:33 +0700

    Perubahan Halaman Landing
    Device Iphone 7 memberikan kesan tumpang tindih di beberapa komponent

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,7 @@ const menuBar = document.querySelector('.menu-bar');
 const headerShrink = document.querySelector('.header');
 const dashboardContentShrink = document.querySelector('.dashboard-content');
 const contentShrink = document.querySelector('.content');
+const formContentShrink = document.querySelector('.form-content');
 const footerShrink = document.querySelector('.footer');
 
 menuBar.addEventListener('click', () => {
@@ -10,5 +11,6 @@ menuBar.addEventListener('click', () => {
     if (headerShrink) headerShrink.classList.toggle('shrink');
     if (dashboardContentShrink) dashboardContentShrink.classList.toggle('shrink');
     if (contentShrink) contentShrink.classList.toggle('shrink');
+    if (formContentShrink) formContentShrink.classList.toggle('shrink');
     if (footerShrink) footerShrink.classList.toggle('shrink');
 });
