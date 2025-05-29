ba9b86002d407b5c2fa2c1ce5c09730d553deea4
Rafif Muhammad <52074659+rafifmuhammad@users.noreply.github.com>
2025-05-29 22:30:58 +0700

    Front-end Finished
    One of them don't even check it, he only said, it is cool do the integration and shit. He didn't know i missed several pages. What a great Team right? No idea

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,7 +5,11 @@ const dashboardContentShrink = document.querySelector('.dashboard-content');
 const contentShrink = document.querySelector('.content');
 const formContentShrink = document.querySelector('.form-content');
 const footerShrink = document.querySelector('.footer');
+const openModal = document.querySelector('.open-modal');
+const modalAlert = document.querySelector('.modal-alert');
+const closeModal = document.querySelector('.close-modal');
 
+// Navigation
 menuBar.addEventListener('click', () => {
     menuNav.classList.toggle('menu-active');
     if (headerShrink) headerShrink.classList.toggle('shrink');
@@ -13,4 +17,11 @@ menuBar.addEventListener('click', () => {
     if (contentShrink) contentShrink.classList.toggle('shrink');
     if (formContentShrink) formContentShrink.classList.toggle('shrink');
     if (footerShrink) footerShrink.classList.toggle('shrink');
+});
+
+
+// Modal Error
+
+openModal.addEventListener('click', () => {
+    if (modalAlert) modalAlert.classList.toggle('modal-active');
 });
